8bf48a851cc4929beb510dac9e964dea310d733b
elisandra <elisandra.lima@souunit.com.br>
2024-05-05 21:46:58 -0300

    atualizacao Q1

--- exercicios/Q1.py
+++ exercicios/Q1.py
@@ -1,3 +1,4 @@
+#cotacao
 carteira = float(input('Quanto você tem na carteira em reais?'))
 print('Você pode comprar em dólar:', round(carteira / 5.23, 2)) # dia da cotação 21/04
 print('Você pode comprar em euros:', round(carteira / 5.57, 2))# dia da cotação 21/04
